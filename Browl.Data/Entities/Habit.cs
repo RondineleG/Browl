a351cbaf0c513a529d64d0da890833d54dbfaff8
Rondinele Guimarães <rondineleg@gmail.com>
2023-08-24 20:38:27 +0000

    Upadate model

--- Browl.Data/Entities/Habit.cs
+++ Browl.Data/Entities/Habit.cs
@@ -1,7 +1,10 @@
+using Browl.Data.Interfaces;
+
 namespace Browl.Data.Entities;
-public class Habit
+public class Habit: IHasTenant
 {
   public int Id { get; set; }
   public string Name { get; set; } = default!;
   public string Description { get; set; } = default!;
+  public string TenantName { get; set; } = default!;
 }
---
+++ Browl.Data/Interfaces/IHasTenant.cs
@@ -0,0 +1,5 @@
+namespace Browl.Data.Interfaces;
+public interface IHasTenant
+{
+    public string TenantName { get; set; }
+}

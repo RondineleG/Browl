08f391c69aa9f25d6592c36e056af6ac8f1ca758
Rondinele Guimarães <rondineleg@gmail.com>
2023-08-24 21:06:47 +0000

    Add TenantService

--- Browl.Data/BrowlDbContext.cs
+++ Browl.Data/BrowlDbContext.cs
@@ -2,7 +2,7 @@ using Browl.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Browl.Data;
-public class BrowlDbContext : DbContext
+public class BrowlDbContext : DBContext
 {
     public DbSet<Habit>? Habits { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=sqlserver;Database=GoodHabitsDatabase;User Id=sa;Password=Password1 ;Integrated Security=false;TrustServerCertificate=true;");
---
+++ Browl.Data/TenantSettings.cs
@@ -0,0 +1,12 @@
+namespace Browl.Data;
+
+public class TenantSettings
+{
+    public string? DefaultConnectionString { get; set; }
+    public List<Tenant>? Tenants { get; set; }
+}
+public class Tenant
+{
+    public string? TenantName { get; set; }
+    public string? ConnectionString { get; set; }
+}

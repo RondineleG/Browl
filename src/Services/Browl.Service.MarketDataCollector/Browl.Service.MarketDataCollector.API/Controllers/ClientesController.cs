19663235c54a72d2fcb81aa3b1663c8d7ada8191
Solid Computing Machine <dev@cia3r.com.br>
2023-09-05 10:43:56 -0300

    Fix name references to new modified names

--- src/Services/Browl.Service.MarketDataCollector/Browl.Service.MarketDataCollector.API/Controllers/ClientesController.cs
+++ src/Services/Browl.Service.MarketDataCollector/Browl.Service.MarketDataCollector.API/Controllers/ClientesController.cs
@@ -22,12 +22,12 @@ public class ClientesController : ControllerBase
     /// Retorna todos clientes cadastrados na base.
     /// </summary>
     [HttpGet]
-    [ProducesResponseType(typeof(CustomerView), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomerViewResource), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Get()
     {
-        var clientes = await clienteManager.GetClientesAsync();
+        var clientes = await clienteManager.GetAsync();
         if (clientes.Any())
         {
             return Ok(clientes);
@@ -40,12 +40,12 @@ public class ClientesController : ControllerBase
     /// </summary>
     /// <param name="id" example="123">Id do cliente.</param>
     [HttpGet("{id}")]
-    [ProducesResponseType(typeof(CustomerView), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomerViewResource), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Get(int id)
     {
-        var cliente = await clienteManager.GetClienteAsync(id);
+        var cliente = await clienteManager.GetAsync(id);
         if (cliente.Id == 0)
         {
             return NotFound();
@@ -58,18 +58,18 @@ public class ClientesController : ControllerBase
     /// </summary>
     /// <param name="novoCliente"></param>
     [HttpPost]
-    [ProducesResponseType(typeof(CustomerView), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(CustomerViewResource), StatusCodes.Status201Created)]
     [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Post(CustomerResource novoCliente)
     {
         logger.LogInformation("Objeto recebido {@novoCliente}", novoCliente);
 
-        CustomerView clienteInserido;
+        CustomerViewResource clienteInserido;
         using (Operation.Time("Tempo de adição de um novo cliente."))
         {
             logger.LogInformation("Foi requisitada a inserção de um novo cliente.");
-            clienteInserido = await clienteManager.InsertClienteAsync(novoCliente);
+            clienteInserido = await clienteManager.PostAsync(novoCliente);
         }
         return CreatedAtAction(nameof(Get), new { id = clienteInserido.Id }, clienteInserido);
     }
@@ -79,12 +79,12 @@ public class ClientesController : ControllerBase
     /// </summary>
     /// <param name="alteraCliente"></param>
     [HttpPut]
-    [ProducesResponseType(typeof(CustomerView), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(CustomerViewResource), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Put(CustomerUpdateResource alteraCliente)
     {
-        var clienteAtualizado = await clienteManager.UpdateClienteAsync(alteraCliente);
+        var clienteAtualizado = await clienteManager.PutAsync(alteraCliente);
         if (clienteAtualizado == null)
         {
             return NotFound();
@@ -103,7 +103,7 @@ public class ClientesController : ControllerBase
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> Delete(int id)
     {
-        var clienteExcliudo = await clienteManager.DeleteClienteAsync(id);
+        var clienteExcliudo = await clienteManager.DeleteAsync(id);
         if (clienteExcliudo == null)
         {
             return NotFound();

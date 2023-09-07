9804d4dda880698ea62386e50c3fd327af9cb40c
RondineleG <rondineleg@outlook.com>
2023-09-07 20:50:13 -0300

    Change to use var

--- src/Presentation/Browl.Client/Program.cs
+++ src/Presentation/Browl.Client/Program.cs
@@ -3,14 +3,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+	BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 builder.Services.AddMudServices();
 

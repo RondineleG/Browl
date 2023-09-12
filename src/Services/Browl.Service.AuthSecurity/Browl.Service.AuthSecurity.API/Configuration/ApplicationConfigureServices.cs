b05e04cd2ecf28e7e8fc501528c1c87af9e4bf67
RondineleG <rondineleg@outlook.com>
2023-09-12 20:24:12 -0300

    Change to use IdentityDbContext<User>

---
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.API/Configuration/ApplicationConfigureServices.cs
@@ -0,0 +1,21 @@
+﻿using Browl.Service.AuthSecurity.API.Data;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Browl.Service.AuthSecurity.API.Configuration;
+
+public static class ApplicationConfigureServices
+{
+
+    public static IServiceCollection ConfigureDependenciesServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BrowlAuthSecurityDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        services.AddIdentity<BrowlAuthSecurityDbContext, IdentityRole>()
+                .AddEntityFrameworkStores<BrowlAuthSecurityDbContext>()
+                .AddDefaultUI()
+                .AddDefaultTokenProviders();
+
+        return services;
+    }
+}

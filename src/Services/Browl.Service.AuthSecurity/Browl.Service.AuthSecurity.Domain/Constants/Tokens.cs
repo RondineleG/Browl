c2d2e80523c8f476859e0c82070c09b61659f562
RondineleG <rondineleg@outlook.com>
2023-09-12 20:06:49 -0300

    Implement JWT Authentication new method for compare

---
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Domain/Constants/Tokens.cs
@@ -0,0 +1,8 @@
+﻿namespace Browl.Service.AuthSecurity.Domain.Constants
+{
+    public class Tokens
+    {
+        public string Token { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Domain/Constants/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace Browl.Service.AuthSecurity.Domain.Constants
+{
+    public class Users
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}

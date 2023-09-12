2fd589177729a6c17d2297c32a92bed46eb85c8c
RondineleG <rondineleg@outlook.com>
2023-09-12 20:11:55 -0300

    Modify the default  names of the Identity Tables

--- src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.API/Data/BrowlAuthSecurityDbContext.cs
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.API/Data/BrowlAuthSecurityDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Browl.Service.AuthSecurity.API.Data;
@@ -6,4 +7,38 @@ namespace Browl.Service.AuthSecurity.API.Data;
 public class BrowlAuthSecurityDbContext : IdentityDbContext
 {
     public BrowlAuthSecurityDbContext(DbContextOptions<BrowlAuthSecurityDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.HasDefaultSchema("Identity");
+        builder.Entity<IdentityUser>(entity =>
+        {
+            entity.ToTable(name: "User");
+        });
+        builder.Entity<IdentityRole>(entity =>
+        {
+            entity.ToTable(name: "Role");
+        });
+        builder.Entity<IdentityUserRole<string>>(entity =>
+        {
+            entity.ToTable("UserRoles");
+        });
+        builder.Entity<IdentityUserClaim<string>>(entity =>
+        {
+            entity.ToTable("UserClaims");
+        });
+        builder.Entity<IdentityUserLogin<string>>(entity =>
+        {
+            entity.ToTable("UserLogins");
+        });
+        builder.Entity<IdentityRoleClaim<string>>(entity =>
+        {
+            entity.ToTable("RoleClaims");
+        });
+        builder.Entity<IdentityUserToken<string>>(entity =>
+        {
+            entity.ToTable("UserTokens");
+        });
+    }
 }

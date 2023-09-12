328b47e25b78d6e4513c1210f1dd92fa4cf79a0a
RondineleG <rondineleg@outlook.com>
2023-09-12 20:15:47 -0300

    Move to entitties

--- src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Domain/Constants/Users.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Browl.Service.AuthSecurity.Domain.Constants
-{
-    public class Users
-    {
-        public string Name { get; set; }
-        public string Password { get; set; }
-    }
-}

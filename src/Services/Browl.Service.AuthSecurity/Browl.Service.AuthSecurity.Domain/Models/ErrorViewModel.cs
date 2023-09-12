b714c8fa557a41eaa0cac298b76a6535484a56d7
RondineleG <rondineleg@outlook.com>
2023-09-12 20:23:27 -0300

    Add inputmodel and change do file scoped

--- src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Domain/Models/ErrorViewModel.cs
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Domain/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace Browl.Service.AuthSecurity.Domain.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
+namespace Browl.Service.AuthSecurity.Domain.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }

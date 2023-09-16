e9bfe8c6b955f5d5d152a8740e47e9d9a665e4e2
RondineleG <rondineleg@outlook.com>
2023-09-15 23:11:55 -0300

    Change BrowlAuthSecurityDbContext to add OnModelCreating to rename identity tableas

--- src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.API/Data/BrowlAuthSecurityDbContext.cs
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.API/Data/BrowlAuthSecurityDbContext.cs
@@ -1,15 +1,54 @@
 ﻿using Browl.Service.AuthSecurity.Domain.Entities;
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Browl.Service.AuthSecurity.API.Data;
 
 public class BrowlAuthSecurityDbContext : IdentityDbContext<User>
-{ 
-    public BrowlAuthSecurityDbContext(DbContextOptions<BrowlAuthSecurityDbContext> options) : base(options)
-    {
-    }
+{
+	public BrowlAuthSecurityDbContext(DbContextOptions<BrowlAuthSecurityDbContext> options)
+		: base(options)
+	{
+	}
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		base.OnModelCreating(builder);
+		builder.HasDefaultSchema("Identity");
+		builder.Entity<User>(entity =>
+		{
+			entity.ToTable(name: "User");
+		});
+
+		builder.Entity<IdentityRole>(entity =>
+		{
+			entity.ToTable(name: "Role");
+		});
+		builder.Entity<IdentityUserRole<string>>(entity =>
+		{
+			entity.ToTable("UserRoles");
+		});
+
+		builder.Entity<IdentityUserClaim<string>>(entity =>
+		{
+			entity.ToTable("UserClaims");
+		});
+
+		builder.Entity<IdentityUserLogin<string>>(entity =>
+		{
+			entity.ToTable("UserLogins");
+		});
+
+		builder.Entity<IdentityRoleClaim<string>>(entity =>
+		{
+			entity.ToTable("RoleClaims");
+
+		});
+
+		builder.Entity<IdentityUserToken<string>>(entity =>
+		{
+			entity.ToTable("UserTokens");
+		});
+	}
 }
-
-
---
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.API/Data/ContextSeed.cs
@@ -0,0 +1,45 @@
+﻿using Browl.Service.AuthSecurity.Domain.Entities;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace Browl.Service.AuthSecurity.API.Data
+{
+	public static class ContextSeed
+	{
+		public static async Task SeedRolesAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			//Seed Roles
+			await roleManager.CreateAsync(new IdentityRole(Enums.Roles.SuperAdmin.ToString()));
+			await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Admin.ToString()));
+			await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Moderator.ToString()));
+			await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Basic.ToString()));
+		}
+		public static async Task SeedSuperAdminAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			//Seed Default User
+			var defaultUser = new User
+			{
+				UserName = "rondineleg",
+				Email = "superadmin@gmail.com",
+				FirstName = "Rondinele",
+				LastName = "Guimaraes",
+				UserNameChangeLimit = 10,
+				EmailConfirmed = true,
+				PhoneNumberConfirmed = true
+			};
+			if (userManager.Users.All(u => u.Id != defaultUser.Id))
+			{
+				var user = await userManager.FindByEmailAsync(defaultUser.Email);
+				if (user == null)
+				{
+					await userManager.CreateAsync(defaultUser, "123Pa$$word.");
+					await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Basic.ToString());
+					await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Moderator.ToString());
+					await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Admin.ToString());
+					await userManager.AddToRoleAsync(defaultUser, Enums.Roles.SuperAdmin.ToString());
+				}
+
+			}
+		}
+	}
+}

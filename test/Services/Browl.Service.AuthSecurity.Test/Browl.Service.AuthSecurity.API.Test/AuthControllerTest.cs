eb5cd1c92efa1e14e02e6a49457df996decf9e77
RondineleG <rondineleg@outlook.com>
2023-09-17 15:05:09 -0300

    feat : Create a new projext test for Browl.Service.AuthSecurity.API

---
+++ test/Services/Browl.Service.AuthSecurity.Test/Browl.Service.AuthSecurity.API.Test/AuthControllerTest.cs
@@ -0,0 +1,63 @@
+﻿using Browl.Service.AuthSecurity.API.Controllers;
+using Browl.Service.AuthSecurity.API.Entities;
+using Browl.Service.AuthSecurity.API.Test.Builder;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+using NSubstitute;
+
+namespace Browl.Service.AuthSecurity.API.Test
+{
+	public class AuthControllerTest
+	{
+		private readonly AuthController _authController;
+		private readonly SignInManager<IdentityUser> _signInManager;
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly IOptions<AppSettings> _appSettings;
+
+		public AuthControllerTest()
+		{
+			_signInManager = Substitute.For<SignInManager<IdentityUser>>(
+				Substitute.For<UserManager<IdentityUser>>(
+					Substitute.For<IUserStore<IdentityUser>>(), null, null, null, null, null),
+				Substitute.For<IHttpContextAccessor>(),
+				Substitute.For<IUserClaimsPrincipalFactory<IdentityUser>>(),
+				null, null, null, null);
+
+			_userManager = Substitute.For<UserManager<IdentityUser>>(
+				Substitute.For<IUserStore<IdentityUser>>(),
+				null, null, null, null, null, null, null, null);
+
+			_appSettings = Substitute.For<IOptions<AppSettings>>();
+			_appSettings.Value.Returns(new AppSettings());
+
+			_authController = new AuthController(_signInManager, _userManager, _appSettings);
+		}
+
+		[Fact]
+		public async Task Register_IsValidModel_ReturnsOkResult()
+		{
+			// Arrange
+			var randomUser = RandomUserGenerator.GenerateRandomUser();
+
+			// Configurar o mock para retornar IdentityResult.Success
+			_userManager.CreateAsync(Arg.Any<IdentityUser>(), Arg.Any<string>())
+				.Returns(Task.FromResult(IdentityResult.Success));
+
+			// Act
+			var result = await _authController.Register(randomUser);
+
+			// Assert
+			var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+			var userResponse = okResult.Value.Should().BeAssignableTo<UserResponse>().Subject;
+
+			// Adicione asserções adicionais, se necessário
+		}
+
+	}
+}
---
+++ test/Services/Browl.Service.AuthSecurity.Test/Browl.Service.AuthSecurity.API.Test/Builder/RandomUserGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+using Browl.Service.AuthSecurity.API.Entities;
+
+namespace Browl.Service.AuthSecurity.API.Test.Builder;
+
+public static class RandomUserGenerator
+{
+	private static readonly Random Random = new Random();
+
+	public static UserRegister GenerateRandomUser()
+	{
+		var userName = GenerateRandomString(8);
+		var firstName = GenerateRandomString(6);
+		var lastName = GenerateRandomString(6);
+		var userNameChangeLimit = Random.Next(0, 10);
+		var profilePicture = GenerateRandomBytes(64); // Tamanho do perfil da imagem em bytes
+		var email = GenerateRandomEmail();
+		var password = GenerateRandomString(10);
+		var passwordConfirmation = password;
+
+		return new UserRegister(userName, firstName, lastName, userNameChangeLimit, profilePicture, email, password, passwordConfirmation);
+	}
+
+	private static string GenerateRandomString(int length)
+	{
+		const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		return new string(Enumerable.Repeat(chars, length)
+			.Select(s => s[Random.Next(s.Length)])
+			.ToArray());
+	}
+
+	private static byte[] GenerateRandomBytes(int length)
+	{
+		var buffer = new byte[length];
+		Random.NextBytes(buffer);
+		return buffer;
+	}
+
+	private static string GenerateRandomEmail()
+	{
+		string[] domains = { "example.com", "test.org", "sample.net" };
+		var userName = GenerateRandomString(8);
+		var domain = domains[Random.Next(domains.Length)];
+		return $"{userName}@{domain}";
+	}
+}

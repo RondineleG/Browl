86ea7be586e4fd249f3b513ac6b7c63497c81002
RondineleG <rondineleg@outlook.com>
2023-09-21 22:20:30 -0300

    feat:  Adjustind test method

--- test/Services/Browl.Service.AuthSecurity.Test/Browl.Service.AuthSecurity.API.Test/AuthControllerTest.cs
+++ test/Services/Browl.Service.AuthSecurity.Test/Browl.Service.AuthSecurity.API.Test/AuthControllerTest.cs
@@ -1,5 +1,7 @@
 ﻿using Browl.Service.AuthSecurity.API.Controllers;
 using Browl.Service.AuthSecurity.API.Entities;
+using Browl.Service.AuthSecurity.API.Services;
+using Browl.Service.AuthSecurity.API.Services.Interfaces;
 using Browl.Service.AuthSecurity.API.Test.Builder;
 
 using FluentAssertions;
@@ -13,13 +15,13 @@ using NSubstitute;
 
 namespace Browl.Service.AuthSecurity.API.Test
 {
-	public class AuthControllerTest
+	public class AuthControllerTest 
 	{
 		private readonly AuthController _authController;
 		private readonly SignInManager<IdentityUser> _signInManager;
 		private readonly UserManager<IdentityUser> _userManager;
 		private readonly IOptions<AppSettings> _appSettings;
-
+		private readonly IAuthenticateService _authenticateService;
 		public AuthControllerTest()
 		{
 			_signInManager = Substitute.For<SignInManager<IdentityUser>>(
@@ -33,10 +35,11 @@ namespace Browl.Service.AuthSecurity.API.Test
 				Substitute.For<IUserStore<IdentityUser>>(),
 				null, null, null, null, null, null, null, null);
 
-			_appSettings = Substitute.For<IOptions<AppSettings>>();
 			_appSettings.Value.Returns(new AppSettings());
 
-			_authController = new AuthController(_signInManager, _userManager, _appSettings);
+
+			_authController = new AuthController(_signInManager, _userManager, _appSettings, _authenticateService);
+
 		}
 
 		[Fact]
@@ -56,7 +59,6 @@ namespace Browl.Service.AuthSecurity.API.Test
 			var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
 			var userResponse = okResult.Value.Should().BeAssignableTo<UserResponse>().Subject;
 
-			// Adicione asserções adicionais, se necessário
 		}
 
 	}

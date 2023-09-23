1674d7cc09431a74218634850ce58f5afd4207b1
RondineleG <rondineleg@outlook.com>
2023-09-23 00:05:19 -0300

    feat: Create new project to manager users with identity

---
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Identity/Services/UserService.cs
@@ -0,0 +1,47 @@
+﻿using Browl.Service.AuthSecurity.Application.Contracts.Identity;
+using Browl.Service.AuthSecurity.Application.Models.Identity;
+using Browl.Service.AuthSecurity.Identity.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace Browl.Service.AuthSecurity.Identity.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public UserService(UserManager<ApplicationUser> userManager, IHttpContextAccessor contextAccessor)
+        {
+            _userManager = userManager;
+            _contextAccessor = contextAccessor;
+        }
+
+        public string UserId { get => _contextAccessor.HttpContext?.User?.FindFirstValue("uid"); }
+
+        public async Task<Employee> GetEmployee(string userId)
+        {
+            var employee = await _userManager.FindByIdAsync(userId);
+            return new Employee
+            {
+                Email = employee.Email,
+                Id = employee.Id,
+                Firstname = employee.FirstName,
+                Lastname = employee.LastName
+            };
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            var employees = await _userManager.GetUsersInRoleAsync("Employee");
+            return employees.Select(q => new Employee
+            {
+                Id = q.Id,
+                Email = q.Email,
+                Firstname = q.FirstName,
+                Lastname = q.LastName
+            }).ToList();
+        }
+    }
+}

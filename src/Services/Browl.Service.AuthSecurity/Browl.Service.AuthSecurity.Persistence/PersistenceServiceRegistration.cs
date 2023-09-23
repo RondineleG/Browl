e3c434c200f844ca95121e4e82acf05a6da44271
RondineleG <rondineleg@outlook.com>
2023-09-23 00:01:52 -0300

    feat: Add new project to persitence database

---
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Browl.Service.AuthSecurity.Application.Contracts.Persistence;
+using Browl.Service.AuthSecurity.Persistence.DatabaseContext;
+using Browl.Service.AuthSecurity.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Browl.Service.AuthSecurity.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<HrDatabaseContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        return services;
+    }
+}

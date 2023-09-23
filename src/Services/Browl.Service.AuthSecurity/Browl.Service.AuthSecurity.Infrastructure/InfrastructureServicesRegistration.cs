4a190335546452b2a6aa688885fc82ad56fda17e
RondineleG <rondineleg@outlook.com>
2023-09-23 00:03:13 -0300

    feat: Add new project infraestructure for send email, logs

---
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Browl.Service.AuthSecurity.Application.Contracts.Email;
+using Browl.Service.AuthSecurity.Application.Contracts.Logging;
+using Browl.Service.AuthSecurity.Application.Models.Email;
+using Browl.Service.AuthSecurity.Infrastructure.EmailService;
+using Browl.Service.AuthSecurity.Infrastructure.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Browl.Service.AuthSecurity.Infrastructure;
+
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.AddScoped(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+            return services;
+        }
+    }
---
+++ src/Services/Browl.Service.AuthSecurity/Browl.Service.AuthSecurity.Infrastructure/Logging/LoggerAdapter.cs
@@ -0,0 +1,24 @@
+﻿using Browl.Service.AuthSecurity.Application.Contracts.Logging;
+using Microsoft.Extensions.Logging;
+
+namespace Browl.Service.AuthSecurity.Infrastructure.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+    }
+}

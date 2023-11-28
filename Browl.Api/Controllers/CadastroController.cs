8d4857621c92a754c5866dba06107cccb8be4e6b
Maik <kiamdemouralechinovski@hotmail.com>
2023-11-27 23:02:30 -0300

    Realizando tratamento de campos vazios na tela de cadastro.

---
+++ Browl.Api/Controllers/CadastroController.cs
@@ -0,0 +1,21 @@
+﻿using Browl.MVC.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Browl.MVC.Controllers;
+public class CadastroController : Controller
+{
+	[HttpPost]
+	public ActionResult Cadastro(string email, string nome, string password)
+	{
+		if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(password))
+		{
+			TempData["CadastroAuthenticateError"] = "Todos os campos devem ser preenchidos";
+			return RedirectToAction("cadastro", "Login");
+		}
+
+		// Lógica para processar o cadastro quando os campos são preenchidos		
+
+		return RedirectToAction("Sucesso"); // Redireciona para uma página de sucesso
+	}
+}
